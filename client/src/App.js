2e09ecde652f148d2a26213598d9e430e5b62d85
SirIsaacNeutron <dbadir@cox.net>
2020-09-04 13:06:39 -0700

    Implemented error reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store'
 
 import AppNavbar from './components/AppNavbar';
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <AppNavbar />
-          <h1>Hello, world!</h1>
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <AppNavbar />
+            <h1>Hello, world!</h1>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
