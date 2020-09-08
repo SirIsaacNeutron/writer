56fb039add4a22631b586fac61966cfea94b821e
SirIsaacNeutron <dbadir@cox.net>
2020-09-07 23:01:31 -0700

    Preparing for Heroku deployment
    I also added compression middleware since it can make the website faster
    from what I understand.

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
 
 const userRoutes = require('./routes/api/users');
 const authRoutes = require('./routes/api/auth');
 const postRoutes = require('./routes/api/posts');
 
 const app = express();
+
+app.use(compression());
 
 app.use(express.json());
 
@@ -32,6 +36,14 @@ const corsOptions = {
             callback(new Error(`${origin} not allowed by CORS`));
         }
     }
+}
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 app.use(cors(corsOptions));
